f095bb2f0b79bcb435daf155460c715f1b945cdb
sss160 <sss160@student.bham.ac.uk>
2023-02-09 16:30:25 +0400

    add name 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main (String [] args) {
-        System.out.println("hello world bareera zakir warisa");
+        System.out.println("hello world bareera zakir warisa areeba");
 
     }
 
